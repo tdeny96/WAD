4ca820891b5e545a7292adc7715f6ae83a029a8b
tdeny96 <32493032+tdeny96@users.noreply.github.com>
2017-12-04 14:58:15 +0200

    Request form added.

---
+++ html/request.php
@@ -0,0 +1,6 @@
+<?php 
+You have requested the echo htmlspecialchars($_POST['name']) by  echo htmlspecialchars($_POST['author']).
+You have also requested the echo htmlspecialchars($_POST['song']) by  echo htmlspecialchars($_POST['artist']) on 
+the page echo htmlspecialchars($_POST['page']).
+Thank you very much.
+?>
